64ea59792296636bcd7bb9a6784eb72bf356ed1a
gevo222 <51228298+gevo222@users.noreply.github.com>
2021-09-14 19:54:51 -0700

    Added Project/Employee relationship

--- src/main/java/com/gevo/pma/entities/Employee.java
+++ src/main/java/com/gevo/pma/entities/Employee.java
@@ -3,6 +3,8 @@ package com.gevo.pma.entities;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Employee {
@@ -10,38 +12,62 @@ public class Employee {
 	@Id
 	@GeneratedValue
 	private Long employeeId;
-	
+
 	private String firstName;
 	private String lastName;
 	private String email;
-	
-	public Employee() 
-	{
-		
+
+	@ManyToOne
+	@JoinColumn(name = "project_id")
+	private Project project;
+
+	public Employee() {
+
 	}
-	
+
 	public Employee(String firstName, String lastName, String email) {
 		super();
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.email = email;
 	}
-	
+
+	public Long getEmployeeId() {
+		return employeeId;
+	}
+
+	public void setEmployeeId(Long employeeId) {
+		this.employeeId = employeeId;
+	}
+
+	public Project getProject() {
+		return project;
+	}
+
+	public void setProject(Project project) {
+		this.project = project;
+	}
+
 	public String getFirstName() {
 		return firstName;
 	}
+
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
+
 	public String getLastName() {
 		return lastName;
 	}
+
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
