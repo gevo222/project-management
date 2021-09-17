0b26f65d2a0c2b6d8b0739571ffb86044510db3f
gevo222 <51228298+gevo222@users.noreply.github.com>
2021-09-17 00:39:58 -0700

    Now seeding database with SQL instead of java code

--- src/main/java/com/gevo/pma/entities/Employee.java
+++ src/main/java/com/gevo/pma/entities/Employee.java
@@ -6,6 +6,7 @@ import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
@@ -16,7 +17,7 @@ import javax.persistence.ManyToOne;
 public class Employee {
 
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long employeeId;
 
 	private String firstName;
