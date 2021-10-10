31f6dc6cdd6020ff4d7ea57848fdb24bc42e7f2e
gevo222 <51228298+gevo222@users.noreply.github.com>
2021-10-10 03:10:16 -0700

    Added user authentication, authorization and password encoding

---
+++ src/main/java/com/gevo/pma/security/WebConfig.java
@@ -0,0 +1,16 @@
+package com.gevo.pma.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class WebConfig {
+
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder()
+	{
+		BCryptPasswordEncoder bCrytpoEncoder = new BCryptPasswordEncoder();
+		return bCrytpoEncoder;
+	}
+}
