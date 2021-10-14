1100407bc6a5898a1a298cc2e0e89b56e0bde0e9
gevo222 <51228298+gevo222@users.noreply.github.com>
2021-10-14 06:07:27 -0700

    Disabled csrf for development

--- src/main/java/com/gevo/pma/security/SecurityConfiguration.java
+++ src/main/java/com/gevo/pma/security/SecurityConfiguration.java
@@ -35,6 +35,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 	@Override
 	protected void configure(HttpSecurity http) throws Exception
 	{
+		http.csrf().disable(); //disabled for testing because POST would not work
 		http.authorizeRequests()
 		.antMatchers("/projects/new").hasRole("ADMIN")
 		.antMatchers("/projects/save").hasRole("ADMIN")
